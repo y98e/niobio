45bfb5f473ae6a435dee424cdf82e0cc8fde85b2
soldate <soldate@gmail.com>
2020-12-02 12:30:21 -0300

    solo miner and wallet

---
+++ src/coin/run/RunMiner.java
@@ -0,0 +1,9 @@
+package coin.run;
+
+public abstract class RunMiner extends Run {
+
+	// git update and run it again
+	public static void main(final String[] args) {
+		run("coin.run.MinerOnly");
+	}
+}
---
+++ src/coin/run/RunWallet.java
@@ -0,0 +1,9 @@
+package coin.run;
+
+public abstract class RunWallet extends Run {
+
+	// git update and run it again
+	public static void main(final String[] args) {
+		run("coin.run.WalletOnly");
+	}
+}
