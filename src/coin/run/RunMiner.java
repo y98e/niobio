31d506ea5d8e421d7c666b745b0f9a3c29291155
soldate <soldate@gmail.com>
2020-12-02 12:40:29 -0300

    miner solo param

--- src/coin/run/RunMiner.java
+++ src/coin/run/RunMiner.java
@@ -4,6 +4,6 @@ public abstract class RunMiner extends Run {
 
 	// git update and run it again
 	public static void main(final String[] args) {
-		run("coin.run.MinerOnly");
+		run("coin.run.MinerOnly " + args[0]);
 	}
 }
