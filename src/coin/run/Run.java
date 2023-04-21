0b5c350fae62406d4473baf21c98c149cf701aa6
y98e <45777274+y98e@users.noreply.github.com>
2023-04-21 16:02:48 +0800

    Update Run.java

--- src/coin/run/Run.java
+++ src/coin/run/Run.java
@@ -14,7 +14,7 @@ public class Run {
 				p.waitFor();
 
 				p = Util.exec(
-						"javac --module-path ./lib/bcprov-jdk15on-1.66.jar:./lib/postgresql-42.2.18.jar ./src/coin/crypto/*.java ./src/coin/daemon/*.java ./src/coin/miner/*.java ./src/coin/run/*.java ./src/coin/util/*.java ./src/coin/wallet/*.java ./src/org/json/*.java -d ./bin");
+						"javac -cp ./lib/bcprov-jdk15on-1.66.jar:./lib/postgresql-42.2.18.jar ./src/coin/crypto/*.java ./src/coin/daemon/*.java ./src/coin/miner/*.java ./src/coin/run/*.java ./src/coin/util/*.java ./src/coin/wallet/*.java ./src/org/json/*.java -d ./bin");
 				p.waitFor();
 
 				p = Util.exec(
